d428af28ee980578768bddda479a80c3df4cbbf2
ash <ashddev@proton.me>
2025-04-22 12:12:28 +0100

    benchmark setup time

---
+++ benches/setup.rs
@@ -0,0 +1,65 @@
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use zk_ballot_validation::ballot_validation::{rated_voting, ranked_voting};
+
+fn benchmark_setup(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Setup");
+
+    let range = (-10, 10);
+    let max_credits = 1;
+
+    for &ballot_size in &[4, 8, 16, 32, 64, 128, 256] {
+        group.bench_with_input(
+            BenchmarkId::new("No Budget Rated Voting", ballot_size),
+            &ballot_size,
+            |b, &_size| {
+                b.iter(|| {
+                    let setup_params = rated_voting::no_budget::setup(range, ballot_size, None).expect("Failed to set up no-budget voting parameters");
+                    let _ = black_box(setup_params);
+                });
+            },
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("Max Budget Rated Voting", ballot_size),
+            &ballot_size,
+            |b, &_size| {
+                b.iter(|| {
+                    let setup_params = rated_voting::max_budget::setup(max_credits, ballot_size, None).expect("Failed to set up max-budget voting parameters");
+                    let _ = black_box(setup_params);
+                });
+            },
+        );
+
+        let vec_a: Vec<u32> = (0..ballot_size).map(|x| x as u32).collect();
+
+        group.bench_with_input(
+            BenchmarkId::new("Ranked Voting", ballot_size),
+            &ballot_size,
+            |b, &_size| {
+                b.iter(|| {
+                    let input_vec = vec_a.clone();
+                    let setup_params = ranked_voting::setup(ballot_size, input_vec);
+                    let _ = black_box(setup_params);
+                });
+            },
+        );
+    }
+
+    group.finish();
+}
+
+fn configure_criterion() -> Criterion {
+    Criterion::default()
+        .sample_size(10)
+        .noise_threshold(0.05)
+        .significance_level(0.05)
+        .confidence_level(0.95)
+}
+
+criterion_group! {
+    name = benches;
+    config = configure_criterion();
+    targets = benchmark_setup
+}
+
+criterion_main!(benches);
